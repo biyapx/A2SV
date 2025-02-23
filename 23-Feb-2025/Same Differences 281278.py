5366ddb6f081915ab70ca35e525590f252c2493e
biya <ayalkbettesfahun@gmail.com>
2025-02-23 22:34:34 +0300

    Created 'Same Differences' by A2SV Hub

---
+++ 23-Feb-2025/Same Differences 281278.py
@@ -0,0 +1,19 @@
+# Problem: Same Differences - https://codeforces.com/problemset/problem/1520/D
+
+def solve():
+    n = int(input())
+    a = list(map(int, input().split()))
+    
+    count = {}
+    pairs = 0
+    
+    for i in range(n):
+        diff = a[i] - (i + 1)
+        pairs += count.get(diff, 0)
+        count[diff] = count.get(diff, 0) + 1
+            
+    return pairs
+
+t = int(input())
+for _ in range(t):
+    print(solve())
