24eb4b458067aacf17be24a34a4e245a3f9d7fd0
biya <ayalkbettesfahun@gmail.com>
2025-03-03 10:26:37 +0300

    Created 'A. Beautiful Matrix' by A2SV Hub

---
+++ 03-Mar-2025/A. Beautiful Matrix 288594.py
@@ -0,0 +1,17 @@
+# Problem: A. Beautiful Matrix - https://codeforces.com/problemset/problem/263/A
+
+grid = []
+
+
+for i in range(5):
+    val = list(map(int, input().strip().split()))
+    grid.append(val)
+
+row, column = -1, -1
+for i in range(len(grid)):
+    if 1 in grid[i]:
+        column = grid[i].index(1)
+        row = i
+        break  
+distance = abs(2 - row) + abs(2 - column)
+print(distance)
