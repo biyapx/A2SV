9ba22040df3250be9d150b1c744b361d41d435bf
biya <ayalkbettesfahun@gmail.com>
2025-03-03 10:00:51 +0300

    Created 'Fox And Snake' by A2SV Hub

---
+++ 03-Mar-2025/Fox And Snake 288576.py
@@ -0,0 +1,16 @@
+# Problem: Fox And Snake - https://codeforces.com/problemset/problem/510/A
+
+n, m = map(int, input().strip().split())
+result = []
+
+for i in range(n):
+    if i % 2 == 0:
+        result.append('#' * m)
+    else:
+        if i % 4 == 1:
+            result.append('.' * (m - 1) + '#')
+        else:
+            result.append('#' + '.' * (m - 1))
+
+for line in result:
+    print(line)
