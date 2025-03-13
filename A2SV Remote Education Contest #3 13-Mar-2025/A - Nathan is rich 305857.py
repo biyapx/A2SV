dfcebcd26c1b41a34f51ad5abb1fe7e78f7a87ce
biya <ayalkbettesfahun@gmail.com>
2025-03-13 22:44:32 +0300

    Created 'A - Nathan is rich' by A2SV Hub

---
+++ A2SV Remote Education Contest #3 13-Mar-2025/A - Nathan is rich 305857.py
@@ -0,0 +1,14 @@
+# Problem: A - Nathan is rich - https://codeforces.com/gym/588094/problem/A
+
+t = int(input())
+results = []
+
+for _ in range(t):
+    n = int(input())
+    cars = n // 4
+    remaining_wheels = n % 4
+    vehicles = cars + (1 if remaining_wheels > 0 else 0)
+    results.append(vehicles)
+
+for result in results:
+    print(result)
