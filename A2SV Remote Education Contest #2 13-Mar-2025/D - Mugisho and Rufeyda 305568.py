479d8b8d2e374a65b946d08267dda3c817268de2
biya <ayalkbettesfahun@gmail.com>
2025-03-13 20:46:59 +0300

    Created 'D - Mugisho and Rufeyda' by A2SV Hub

---
+++ A2SV Remote Education Contest #2 13-Mar-2025/D - Mugisho and Rufeyda 305568.py
@@ -0,0 +1,22 @@
+# Problem: D - Mugisho and Rufeyda - https://codeforces.com/gym/586622/problem/D
+
+def find_number(n, t):
+    if t == 10:
+        if n == 1:
+            return -1
+        else:
+            return '1' + '0' * (n - 1)
+    
+    start_number = 10 ** (n - 1)
+    
+    if start_number % t == 0:
+        return start_number
+    else:
+        start_number += (t - (start_number % t))
+        if start_number < 10 ** n:
+            return start_number
+        else:
+            return -1
+
+n, t = map(int, input().strip().split())
+print(find_number(n, t))
