5cc8cab1dd2b2fb5607f4515824d9336fec88029
biya <ayalkbettesfahun@gmail.com>
2025-03-29 10:11:53 +0300

    Created 'B - Thousand Sunny's Network Setup' by A2SV Hub

---
+++ A2SV Education Phase I - Contest #6 29-Mar-2025/B - Thousand Sunny's Network Setup 321480.py
@@ -0,0 +1,12 @@
+# Problem: B - Thousand Sunny's Network Setup - https://codeforces.com/gym/594356/problem/B
+
+def max_speed(n, k, speeds):
+    speeds.sort(reverse=True)
+    
+    return speeds[k - 1]
+
+n, k = map(int, input().split())
+speeds = list(map(int, input().split()))
+
+result = max_speed(n, k, speeds)
+print(result)
