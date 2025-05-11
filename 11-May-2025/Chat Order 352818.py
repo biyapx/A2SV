85d42423c60f5b9bbfeb2894c2d5e81f5e3ed194
Ayalkbet Tesfahun <ayalkbettesfahun@gmail.com>
2025-05-11 22:25:19 +0300

    Created 'Chat Order' by A2SV Hub

---
+++ 11-May-2025/Chat Order 352818.py
@@ -0,0 +1,11 @@
+# Problem: Chat Order - https://codeforces.com/problemset/problem/637/B
+
+n = int(input())
+messages = [input().strip() for _ in range(n)]
+seen = set()
+result = []
+for name in reversed(messages):
+    if name not in seen:
+        seen.add(name)
+        result.append(name)
+print('\n'.join(result))
