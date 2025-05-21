a1fff7b3efdaf7a198ff47bb323313c3f49823c1
Ayalkbet Tesfahun <ayalkbettesfahun@gmail.com>
2025-05-21 17:04:08 +0300

    Created 'D - Great Photographer' by A2SV Hub

---
+++ A2SV Remote Contest #4 21-May-2025/D - Great Photographer 358616.py
@@ -0,0 +1,27 @@
+# Problem: D - Great Photographer - https://codeforces.com/gym/590201/problem/D
+
+import math
+
+n, x0 = map(int, input().split())
+
+max_a = -math.inf 
+min_b = math.inf 
+
+for _ in range(n):
+    a, b = map(int, input().split())
+    start = min(a, b)
+    end = max(a, b)
+    max_a = max(max_a, start)
+    min_b = min(min_b, end)
+
+if max_a <= min_b:
+    if max_a <= x0 <= min_b:
+        print(0)
+    else:
+        
+        if x0 < max_a:
+            print(max_a - x0)
+        else:
+            print(x0 - min_b)
+else:
+    print(-1)
