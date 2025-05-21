476df064a50d5a1340bdff147b2616b0e2a04790
Ayalkbet Tesfahun <ayalkbettesfahun@gmail.com>
2025-05-21 15:46:04 +0300

    Created 'A - Harmonic Melody' by A2SV Hub

---
+++ A2SV Remote Contest #4 21-May-2025/A - Harmonic Melody 358601.py
@@ -0,0 +1,21 @@
+# Problem: A - Harmonic Melody - https://codeforces.com/gym/590201/problem/A
+
+t = int(input())
+res = []
+
+for _ in range(t):
+    n = int(input())  
+    melo = list(map(int, input().split()))  
+    valid = True  
+    for i in range(1, len(melo)):
+        if abs(melo[i] - melo[i - 1]) not in {5, 7}:
+            valid = False 
+            break 
+    
+    if valid:
+        res.append("YES")
+    else:
+        res.append("NO")
+
+for result in res:
+    print(result)
