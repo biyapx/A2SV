fac4687d17d2e6c8226f13db9f73ade8a55c96da
Ayalkbet Tesfahun <ayalkbettesfahun@gmail.com>
2025-05-21 15:50:45 +0300

    Created 'B - The best for Geleta' by A2SV Hub

---
+++ A2SV Remote Contest #4 21-May-2025/B - The best for Geleta 358604.py
@@ -0,0 +1,27 @@
+# Problem: B - The best for Geleta - https://codeforces.com/gym/590201/problem/B
+
+def solve():
+    t = int(input())
+    for _ in range(t):
+        n, m = map(int, input().split())
+        a = list(map(int, input().split()))
+        operations = []
+        for __ in range(m):
+            parts = input().split()
+            c = parts[0]
+            l = int(parts[1])
+            r = int(parts[2])
+            operations.append((c, l, r))
+        
+        current_max = max(a)
+        res = []
+        for c, l, r in operations:
+            if l <= current_max <= r:
+                if c == '+':
+                    current_max += 1
+                else:
+                    current_max -= 1
+            res.append(str(current_max))
+        print(' '.join(res))
+
+solve()
