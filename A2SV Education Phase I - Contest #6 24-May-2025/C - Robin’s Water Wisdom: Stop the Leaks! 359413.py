e387097f051d6e84f783c1b2a562b30663220670
Ayalkbet Tesfahun <ayalkbettesfahun@gmail.com>
2025-05-24 14:32:30 +0300

    Created 'C - Robin’s Water Wisdom: Stop the Leaks!' by A2SV Hub

---
+++ A2SV Education Phase I - Contest #6 24-May-2025/C - Robin’s Water Wisdom: Stop the Leaks! 359413.py
@@ -0,0 +1,26 @@
+# Problem: C - Robin’s Water Wisdom: Stop the Leaks! - https://codeforces.com/gym/594356/problem/C
+
+def min_holes_to_block(n, A, B, sizes):
+    s1 = sizes[0]
+    max_size = (s1 * A) / B
+    total_size = sum(sizes)
+    
+    if total_size <= max_size:
+        return 0
+    
+    other_sizes = sizes[1:]
+    other_sizes.sort(reverse=True)
+    
+    blocked_count = 0
+    for size in other_sizes:
+        total_size -= size
+        blocked_count += 1
+        if total_size <= max_size:
+            return blocked_count
+    
+    return blocked_count
+
+n, A, B = map(int, input().split())
+sizes = list(map(int, input().split()))
+
+print(min_holes_to_block(n, A, B, sizes))
